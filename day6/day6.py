daf11bce81aeb0b8d1c4588b7386af9985afa1a2
Tamas Plugor <tamas@arista.com>
2023-12-07 02:39:07 +0000

    day6 p2

--- day6/day6.py
+++ day6/day6.py
@@ -12,6 +12,14 @@ def parse_data(data: list) -> list:
     d = [int(i) for i in d]
     return t, d
 
+def parse_data2(data: list) -> list:
+    '''Parses the data.'''
+    t = data.splitlines()[0].split(':')[1].replace(" ", "")
+    d = data.splitlines()[1].split(':')[1].replace(" ", "")
+    t = int(t)
+    d = int(d)
+    return t, d
+
 def margin_error(t: list, d: list) -> int:
     '''Calculates the margin of error.'''
     margin = 1
@@ -24,16 +32,25 @@ def margin_error(t: list, d: list) -> int:
         margin = margin * len(record)
     return margin
 
-# beat = 1
-# for ctr, ti in enumerate(t):
-#     record = []
-#     for i in range(1,ti+1):
-#         distance = i*(ti-i)
-#         if distance > d[ctr]:
-#             record.append(distance)
-#     beat = beat* len(record)
+def margin_error2(t: int, d: int) -> int:
+    '''Calculates the margin of error.'''
+    margin = 1
+    record = []
+    for i in range(1,t+1):
+        distance = i*(t-i)
+        if distance > d:
+            record.append(distance)
+    margin = margin * len(record)
+    return margin
+
 t, d = parse_data(test_data)
 print(margin_error(t, d))
 
 t, d = parse_data(data)
 print(margin_error(t, d))
+
+t, d = parse_data2(test_data)
+print(margin_error2(t, d))
+
+t, d = parse_data2(data)
+print(margin_error2(t, d))
